f9238e3505357945e9618ff7229494eeee1a1913
gifigueroa <gian.figueroa@mercadolibre.com.co>
2023-08-30 20:16:03 -0500

    Video and Camino ancestral

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
 			sans: ['Gotham SSm A, sans-serif']
 		},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.black,
+			white: colors.white,
+			gray: colors.gray,
+			emerald: colors.emerald,
+			indigo: colors.indigo,
+			yellow: colors.yellow,
+			lime: colors.lime,
+		  },
 		extend: {},
 	},
 	plugins: [],
